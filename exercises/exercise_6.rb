e6f92fcc901be82ef3b28d66b227ca702a455fd9
Mike Auty <m@auty.ca>
2022-01-04 22:19:55 +0000

    added employees:

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,8 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Mike", last_name: "Auty", hourly_rate: 80)
+@store1.employees.create(first_name: "Harry", last_name: "Carey", hourly_rate: 60)
+@store2.employees.create(first_name: "Optimus", last_name: "Prime", hourly_rate: 50)
+@store2.employees.create(first_name: "Bugs", last_name: "Bunny", hourly_rate: 40)
