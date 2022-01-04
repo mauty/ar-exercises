769468784580885ee93da420d81576a52fe3e1a5
Mike Auty <m@auty.ca>
2022-01-04 18:29:41 +0000

    calculate store revenues

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@allstores_revenue = Store.sum("annual_revenue")
+@allstores_average = Store.average("annual_revenue")
+puts "Total Store Revenue: #{@allstores_revenue}"
+puts "Average Store Revenue: #{@allstores_average}"
+
+@number_milliondollar_stores = Store.where("annual_revenue > 1000000").count
+puts "Number of Million Dollar Stores: #{@number_milliondollar_stores}"
